1406d9e6c768d27eb429534bdebf75f8e2392791
ChrisO615 <150743915+ChrisO615@users.noreply.github.com>
2023-11-14 15:54:53 -0500

    Add files via upload

---
+++ predatory.R
@@ -0,0 +1,12 @@
+nematode <- read.csv("nematode.csv")
+
+Predatory <- nematode$Predatory
+
+Site <- nematode$Site
+
+plot(nematode$Predatory, 
+     breaks = 15,
+     col = "purple", 
+     xlab = "Site",
+     ylab = "Frequency",
+     main = "Predatory Nematodes per Site")
